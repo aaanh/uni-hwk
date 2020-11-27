1b7f9f5045b3bec22b06cbfc846181d60295d7ad
HA <hoanganh.theodore@icloud.com>
2020-11-27 01:05:28 -0500

    WIP project

--- f2020/coen_243/project/Headers/Staff.h
+++ f2020/coen_243/project/Headers/Staff.h
@@ -1,10 +1,45 @@
-#ifndef STAFF_H_
-#define STAFF_H_
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 class Staff {
     public:
-    
-    private:
-}
+        Staff(); // constructor no args
+        Staff(); // constructor w/ args
 
-#endif
+        // Getters
+        string GetFirstName();
+        string GetLastName();
+        
+        int GetID();
+        string GetHiredDate();
+        string GetBonusCode();
+        double GetCurrentSalary();
+
+        // Setters
+        void SetFirstName();
+        void SetLastName();
+        void SetID();
+        void SetHiredDate();
+        void SetBonusCode();
+        void SetSalary();
+
+        // Additional methods
+
+        void PrintStaffInfo();
+        double CalculateSalary();
+        string CompareExp();
+
+    private:
+        string f_name;
+        string l_name;
+
+        int id;
+        string phone_number;
+        string hired_date;
+        string bonus_code;
+        double current_salary;
+}
