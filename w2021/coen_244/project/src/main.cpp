31043f4eb603f460940640f6f0e64a9261b7cf75
HA <hoanganh.theodore@icloud.com>
2021-04-05 17:12:37 -0400

    project re-init

---
+++ w2021/coen_244/project/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Black-box reverse engineering of\nOracle of Bacon (oraceofbacon.org) by Patrick Reynolds (piki@github)\n";
+	cout << "This is a part of final project for COEN 244 W2021\n";
+	cout << "Author: Nguyen, Hoang Anh | ID: 40152583 | E-mail: anhhoang.nguyen@concordia.ca\n";
+}
